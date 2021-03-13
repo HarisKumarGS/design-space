e4ac0c30fac83e73ca5c5f762de16e4139e63a95
hgs <gsh2798@gmail.com>
2021-03-13 16:45:54 +0530

    Add Avatar Library

--- design-space/projects/design-space-material/src/lib/components/avatar/avatar.module.ts
+++ design-space/projects/design-space-material/src/lib/components/avatar/avatar.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AvatarComponent } from './avatar/avatar.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
 @NgModule({
   declarations: [AvatarComponent],
   imports: [
+    BrowserModule,
     CommonModule
   ],
   exports: [AvatarComponent]
--- design-space/projects/design-space-material/src/lib/components/avatar/avatar/avatar.component.ts
+++ design-space/projects/design-space-material/src/lib/components/avatar/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ds-avatar',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvatarComponent implements OnInit {
 
+  @Input() image: string = '';
+  @Input() name: string = '';
+
+  initials: string = '';
+
+  showInitials = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (!this.image) {
+      this.showInitials = true;
+      this.createInitials();
+    }
+  }
+
+  createInitials(): void {
+    let initials = '';
+    for (let i = 0; i < this.name.length; i++) {
+      if (this.name.charAt(i) === ' ') {
+        continue;
+      }
+
+      if (this.name.charAt(i) === this.name.charAt(i).toUpperCase()) {
+        initials += this.name.charAt(i);
+
+        if (initials.length === 2) {
+          break;
+        }
+      }
+    }
+
+    this.initials = initials;
   }
 
 }
